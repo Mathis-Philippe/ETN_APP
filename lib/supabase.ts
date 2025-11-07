37396b0b6c67738c91d79c7699518eee355e9470
mathis <mathis.philippe@my-digital-school.org>
2025-11-07 17:00:05 +0100

    change supabase service key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://wjblfsrgavyqkcakkwdr.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqYmxmc3JnYXZ5cWtjYWtrd2RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MzA2MTIsImV4cCI6MjA3NDIwNjYxMn0.uKc7_juDl-GHT5J11yZ4umj2yCbbe9VUKA0hExNouhQ"; // ta clé anon publique
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqYmxmc3JnYXZ5cWtjYWtrd2RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MzA2MTIsImV4cCI6MjA3NDIwNjYxMn0.uKc7_juDl-GHT5J11yZ4umj2yCbbe9VUKA0hExNouhQ"; 
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
