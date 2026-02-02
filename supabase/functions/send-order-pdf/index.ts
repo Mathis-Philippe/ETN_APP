02c3faae45da88e1797b0684c0388944b7dac4bf
mathis <mathis.philippe@my-digital-school.org>
2026-02-02 16:16:59 +0100

    change final resend mail

--- supabase/functions/send-order-pdf/index.ts
+++ supabase/functions/send-order-pdf/index.ts
@@ -33,7 +33,7 @@ Deno.serve(async (req) => {
 
     // Envoi de l’email
     await resend.emails.send({
-      from: "etn@equipement-technique-du-nord.fr", // doit être un email vérifié ResendA
+      from: "commandes@etn-app.fr", // doit être un email vérifié ResendA
       to: "etn@equipement-technique-du-nord.fr",
       subject: `Bon de commande #${orderNumber}`,
       html: `<p>Nouveau bon de commande de ${firstName} ${lastName}</p>`,
